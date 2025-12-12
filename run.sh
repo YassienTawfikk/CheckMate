6905d9d56b702fe2fb0de76c3f7507a38abd107a
Yassien Tawfik <yassien.m.m.tawfik@gmail.com>
2025-12-12 14:27:22 +0200

    Fix login resolution error and restructure project to flat src

--- run.sh
+++ run.sh
@@ -9,13 +9,13 @@ mkdir -p bin
 # Source path is src/main/java
 # Main file is src/main/java/com/checkmate/core/Main.java
 echo "Compiling..."
-javac -d bin -sourcepath src/main/java src/main/java/com/checkmate/core/Main.java
+javac -d bin -sourcepath src src/com/checkmate/core/Main.java
 
 if [ $? -eq 0 ]; then
     echo "Running CheckMate..."
     # Run from bin directory, including resources in classpath
     # Main class is com.checkmate.core.Main
-    java -cp bin:src/main/resources com.checkmate.core.Main
+    java -cp bin:src/resources com.checkmate.core.Main
 else
     echo "Compilation failed."
 fi
